fb45f931c7c62f4fd1a2d8c74264c6be57507c67
DomestiqueBaston <ferdi2x@gmail.com>
2023-05-12 10:57:33 +0200

    Minor update

--- GODOT/SCRIPTS/TMP/PAPA_CAM.gd
+++ GODOT/SCRIPTS/TMP/PAPA_CAM.gd
@@ -6,14 +6,14 @@ var velocity = Vector2()
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += speed
+		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
+		velocity.y -= 1
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
+		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
-	#velocity = velocity.normalized()
+		velocity.x += 1
+	velocity = velocity.normalized() * speed
 	
 func _physics_process(_delta):
 # warning-ignore:return_value_discarded
