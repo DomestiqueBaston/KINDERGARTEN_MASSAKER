320bdfb86a8383a0f043e2e859d57f785454c967
DOMESTIQUE BASTON - [pure label] <ferdi2x@gmail.com>
2023-06-27 17:25:46 +0200

    Fixed issue with persp of character movements

--- GODOT/SCRIPTS/TMP/PAPA_CAM.gd
+++ GODOT/SCRIPTS/TMP/PAPA_CAM.gd
@@ -6,9 +6,9 @@ var velocity = Vector2()
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		velocity.y += .5
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.y -= .5
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
