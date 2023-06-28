e07d8203f9a5a99484bf1209fed199f8f4c406bf
DOMESTIQUE BASTON - [pure label] <ferdi2x@gmail.com>
2023-06-28 07:38:56 +0200

    Updated character movements...

---
+++ GODOT/SCRIPTS/TMP/CAMERA.gd
@@ -0,0 +1,13 @@
+# Source: https://www.youtube.com/watch?v=Jd0en-xC3bk
+extends Camera2D
+
+var TargetNodePath = NodePath("../PAPA_CHARACTERS")
+var target_node
+export (float) var lerpspeed = 0.15
+
+func _ready():
+	target_node = get_node(TargetNodePath)
+	
+func _process(_delta):
+	self.position = lerp(self.position, target_node.position, lerpspeed)
+	
--- GODOT/SCRIPTS/TMP/CHARACTERS.gd
+++ GODOT/SCRIPTS/TMP/CHARACTERS.gd
@@ -1,20 +1,23 @@
 extends KinematicBody2D
 
-var speed = 175
+
+var speed = Vector2(175.0, 87.5)
 var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += .5
+		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= .5
+		velocity.y -= 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	velocity = velocity.normalized() * speed
-	
+#	print(velocity)
+
+
 func _physics_process(_delta):
 # warning-ignore:return_value_discarded
 	get_input()
