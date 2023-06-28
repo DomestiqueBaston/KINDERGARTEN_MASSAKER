3ee0c1d9a452db09a3459d59be06f8ff5e1a2ed7
DOMESTIQUE BASTON - [pure label] <ferdi2x@gmail.com>
2023-06-28 07:59:16 +0200

    Tweak (hm...). It just looks better to me than yesterday.

--- GODOT/SCRIPTS/TMP/CHARACTERS.gd
+++ GODOT/SCRIPTS/TMP/CHARACTERS.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
 
-var speed = Vector2(175.0, 87.5)
+var speed = 175
+var SPEED = Vector2(speed, speed / 1.5)
 var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		velocity.y += .75
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.y -= .75
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	velocity = velocity.normalized() * speed
-#	print(velocity)
+	velocity = velocity.normalized() * SPEED
+	print(velocity)
 
 
 func _physics_process(_delta):
