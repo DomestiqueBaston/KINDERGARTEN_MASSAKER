215d5bab0f594c7cb904ce0bf7e80d6058e13e2d
DOMESTIQUE BASTON - [pure label] <ferdi2x@gmail.com>
2023-07-27 22:30:14 +0200

    Pico update!

--- GODOT/SCRIPTS/TMP/HHH.gd
+++ GODOT/SCRIPTS/TMP/HHH.gd
@@ -8,7 +8,9 @@ func _process(_delta):
 		$Transition_Overlay/Sprite.take_screenshot()
 		$Active_Scene.get_child(0).queue_free()
 		$Active_Scene.add_child(SceneTwo.instance())
-
+	if Input.is_action_just_pressed("ui_home") and Autoload.transition_signal:
+		Autoload.transition_signal = false
+		get_tree().reload_current_scene()
 	if Autoload.transition_signal == true:
 		$Transition_Overlay.hide()
 
