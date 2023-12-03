f3de1f2dfdf2de3cd2919658617a95228a71a571
Geoff Levner <glevner@gmail.com>
2023-12-03 15:18:14 +0100

    Removed old test code

--- GODOT/SCRIPTS/Autoload.gd
+++ GODOT/SCRIPTS/Autoload.gd
@@ -1,18 +1,5 @@
 extends Node
 
-var choice = 0					#choix du pouvoir (DASH par défaut)
-
-var transition_signal = false	#TRUE si Transition_Overlay a fini de jouer
-var scene_changed = true		#TRUE quand on meurt
-var restart_game = false		#TRUE quand on veut rejouer après être mort
-
-var time_before_death = 50.0
-var time_to_die = false			#TRUE quand on meurt (aussi ?)
-
-# SCORE
-var elapsed_time = 0.0
-var best_time = 0.0
-
 const _ignored_keys = [KEY_ALT, KEY_SHIFT, KEY_CONTROL, KEY_META, KEY_MENU]
 
 #
--- GODOT/SCRIPTS/TMP/TRANSITION.gd
+++ GODOT/SCRIPTS/TMP/TRANSITION.gd
@@ -1,15 +1,9 @@
 extends Sprite
 
-onready var animationPlayer = $"../AnimationPlayer"
-export var animation = "Fade"
-
 func take_screenshot():
 	var img = get_viewport().get_texture().get_data()
 	img.flip_y()
 	var screenshot = ImageTexture.new()
 	screenshot.create_from_image(img)
 	texture = screenshot
-	animationPlayer.play("Fade")
-
-func _on_AnimationPlayer_animation_finished(_anim_name):
-	Autoload.transition_signal = true
+	$"../AnimationPlayer".play("Fade")
