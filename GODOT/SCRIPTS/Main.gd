cb7e4a2f1b8edb5072b60c790b86acb3b3a3a2fb
Geoff Levner <glevner@gmail.com>
2023-12-08 15:29:57 +0100

    Added a comment

--- GODOT/SCRIPTS/Main.gd
+++ GODOT/SCRIPTS/Main.gd
@@ -104,6 +104,10 @@ func _on_Transition_Overlay_transition_finished():
 	$Transition_Overlay.hide()
 
 func on_dialogue_finished():
+	# Without this pause, when the dialogue -> menu transition begins in full
+	# screen mode, the menu sometimes appears for a frame before it disappears
+	# and then fades in as it should. Perhaps we should always pause for a
+	# frame before triggering a transition...?
 	yield(get_tree(), "idle_frame")
 	dialogue_seen = true
 	change_state(GameState.MENU)
