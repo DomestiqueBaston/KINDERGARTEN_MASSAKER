4d483a5746a64f6221949b6eda50539563cd10e3
DomestiqueBaston <ferdi2x@gmail.com>
2023-12-11 20:16:32 +0100

    Tests...

---
+++ GODOT/SCRIPTS/TMP/EDUC.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+export(int) var speed = 80
+
+func _physics_process(_delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1.0
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1.0
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += .75
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= .75
+#	The 1.0 and .75 thing doesn't work! But I guess that you get the idea...
+	
+#	velocity = velocity.normalized()
+	if velocity == Vector2.ZERO:
+		pass
+	else:
+		$AnimationTree.set("parameters/Check/blend_position", velocity)
+		var _ignore_collision = move_and_slide(velocity.normalized() * speed)
