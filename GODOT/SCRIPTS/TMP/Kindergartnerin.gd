55b38512d1f7792936dd17e5a89fb80b102ddf02
DomestiqueBaston <ferdi2x@gmail.com>
2024-01-16 09:14:12 +0100

    This time the iso movement test is effective (hm...).
    I'm sure this is not what the code should look like but at least we get the "perspective effect" in the movement.
    As written in the beginning of the code: this is all for test purpose as we are not supposed to play the Kindergartnerin in KM :laughing:

--- GODOT/SCRIPTS/TMP/Kindergartnerin.gd
+++ GODOT/SCRIPTS/TMP/Kindergartnerin.gd
@@ -12,16 +12,16 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1.0
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += .75
+		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= .75
-#	The 1.0 and .75 thing doesn't work! But I guess that you get the idea...
+		velocity.y -= 1
 	
-#	velocity = velocity.normalized()
 	if velocity == Vector2.ZERO:
 		$AnimationTree.get("parameters/playback").travel("Check")
 	else:
 		$AnimationTree.get("parameters/playback").travel("Run")
 		$AnimationTree.set("parameters/Check/blend_position", velocity)
 		$AnimationTree.set("parameters/Run/blend_position", velocity)
-		var _ignore_collision = move_and_slide(velocity.normalized() * speed)
+		velocity = velocity.normalized()
+		velocity.y *= .75 #Not an ideal solution but I did my best...
+		var _ignore_collision = move_and_slide(velocity * speed)
