14878176559dcd68c671af981e364a0f86692fa3
Geoff Levner <glevner@gmail.com>
2024-02-14 16:34:41 +0100

    Two tweaks...
    Enlarged the window in which enemies are spawned initially, so it is not
    so crowded.
    
    When starting the shutdown sequence, wait until the shutdown animation
    actually starts before resetting the overlay animation.

--- GODOT/SCENES/OVERLAYS/Shutdown_Overlay.gd
+++ GODOT/SCENES/OVERLAYS/Shutdown_Overlay.gd
@@ -1,7 +1,12 @@
 extends CanvasLayer
 
+signal animation_started
+
 func start_animation():
 	$AnimationPlayer.play("Screen_Shut_Down")
 
 func reset_animation():
 	$AnimationPlayer.play("RESET")
+
+func _on_AnimationPlayer_animation_started(_anim_name):
+	emit_signal("animation_started")
