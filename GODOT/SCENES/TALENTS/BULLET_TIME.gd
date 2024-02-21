e4b3748999353d07c91d179ab531f1d9933f852b
Geoff Levner <glevner@gmail.com>
2024-02-21 17:14:50 +0100

    Comments.

--- GODOT/SCENES/TALENTS/BULLET_TIME.gd
+++ GODOT/SCENES/TALENTS/BULLET_TIME.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
+# signal emitted when bullet time is over (but not when stop() is called)
 signal done
 
 var running := false
 
+#
+# Starts bullet time, which will last for the given amount of time in seconds.
+#
 func start(duration: float):
 	$AnimationPlayer.play("bullet_time_on")
 	var anim_off = $AnimationPlayer.get_animation("bullet_time_off")
 	$Bullet_Timer.start(max(0, duration - anim_off.length))
 	running = true
 
+#
+# Interrupts bullet time, if it is running. Does nothing otherwise.
+#
 func stop():
 	$AnimationPlayer.stop()
 	$Bullet_Timer.stop()
 	running = false
 
+#
+# Returns true if bullet time is running.
+#
 func is_running() -> bool:
 	return running
 
