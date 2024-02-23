1851a2f3316b8beeed7a6d72ac1ece1a83c78394
Geoff Levner <glevner@gmail.com>
2024-02-23 18:38:41 +0100

    Enemies move!
    For now, they all do the same thing: idle, move, idle, move...

--- GODOT/SCRIPTS/Enemy.gd
+++ GODOT/SCRIPTS/Enemy.gd
@@ -1,19 +1,67 @@
 extends KinematicBody2D
 
 export var char_name: String
+export var speed := Vector2(100, 50)
 export var default_anim := "Idle"
+export var run_anim := "Run"
 
-var blend_param: String
-var time_param: String
+var idle_blend_param: String
+var run_blend_param: String
+var idle_time_param: String
+var run_time_param: String
+var timer: Timer
+var is_running := false
+var state_machine: AnimationNodeStateMachinePlayback
+var direction: Vector2
 
 func _ready():
 	$AnimationTree.active = true
-	blend_param = "parameters/%s/BlendSpace2D/blend_position" % default_anim
-	time_param = "parameters/%s/TimeScale/scale" % default_anim
-	$AnimationTree[blend_param] = Globals.get_random_direction()
+	state_machine = $AnimationTree["parameters/playback"]
+
+	idle_blend_param = "parameters/%s/BlendSpace2D/blend_position" % default_anim
+	run_blend_param = "parameters/%s/BlendSpace2D/blend_position" % run_anim
+	idle_time_param = "parameters/%s/TimeScale/scale" % default_anim
+	run_time_param = "parameters/%s/TimeScale/scale" % run_anim
+
+	timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+	timer.connect("timeout", self, "_on_timer_timeout")
+	timer.start(3 * randf())
+
+	var dir = Globals.get_random_direction()
+	direction = dir.normalized()
+
+	if randf() < 0.5:
+		$AnimationTree[idle_blend_param] = dir
+		state_machine.travel(default_anim)
+	else:
+		$AnimationTree[run_blend_param] = dir
+		state_machine.travel(run_anim)
+
+func _on_timer_timeout():
+	if is_running:
+		$AnimationTree[idle_blend_param] = $AnimationTree[run_blend_param]
+		state_machine.travel(default_anim)
+	else:
+		$AnimationTree[run_blend_param] = $AnimationTree[idle_blend_param]
+		state_machine.travel(run_anim)
+	is_running = not is_running
+	timer.start(2 + 3 * randf())
+
+func _physics_process(_delta):
+	if state_machine.get_current_node() == run_anim:
+		var time_scale = $AnimationTree[run_time_param]
+		if time_scale != 0:
+			var dir = move_and_slide(direction * speed * time_scale)
+			if get_slide_count() > 0:
+				dir = Globals.get_nearest_direction(dir)
+				$AnimationTree[run_blend_param] = dir
+				direction = dir.normalized()
 
 func set_time_scale(scale = 1.0):
-	$AnimationTree[time_param] = scale
+	$AnimationTree[idle_time_param] = scale
+	$AnimationTree[run_time_param] = scale
 
 func freeze(flash=true):
 	set_time_scale(0)
