cb7764dd83b8af6f099f8ad4b6a1948284f779ae
Geoff Levner <glevner@gmail.com>
2024-03-03 18:00:29 +0100

    Comments.

--- GODOT/SCRIPTS/Enemy.gd
+++ GODOT/SCRIPTS/Enemy.gd
@@ -2,6 +2,14 @@ tool
 extends Runner
 class_name Enemy
 
+# Base class for all enemies (teacher and kids). It handles their movement and
+# any behavior that is shared by all of them, such as detecting when the alien
+# is and is not visible. It is assumed that all enemies have the following
+# three nodes: AnimationPlayer (which contains all the animation cycles),
+# CyclePlayer (which pilots the AnimationPlayer) and Flasher (which contains
+# the flash animation that is played whenever the enemy is frozen or unfrozen,
+# for example).
+
 # running speed
 export var speed := Vector2(100, 50)
 
@@ -40,7 +48,7 @@ func _ready():
 
 	start_checking_for_puddles()
 
-	# all characters start out in their default animation, facing in a random
+	# all enemies start out in their default animation, facing in a random
 	# direction
 
 	var dir = Globals.get_random_direction()
@@ -59,15 +67,15 @@ func _ready():
 
 #
 # Called once to set the animation timer the first time. May be overridden by
-# each character subclass.
+# each enemy subclass.
 #
 func init_timer():
 	timer.start(rand_range(2, 5) / $CyclePlayer.get_speed())
 
 #
-# Called when the animation timer times out to update the character's animation
-# state. By default, the character alternates its default and run animations,
-# for 2-5 seconds at a time. But the method may be overridden by each character
+# Called when the animation timer times out to update the enemy's animation
+# state. By default, the enemy alternates its default and run animations, for
+# 2-5 seconds at a time. But the method may be overridden by each enemy
 # subclass.
 #
 func on_timer_timeout():
@@ -78,14 +86,20 @@ func on_timer_timeout():
 	is_running = not is_running
 	timer.start(rand_range(2, 5) / $CyclePlayer.get_speed())
 
+#
+# Calls the tick() method whenever a physics process signal is received.
+# Subclasses should override tick(), if necessary; they should NOT override
+# this method, because in that case BOTH _physics_process() methods would be
+# called (first the base class's, then the subclass's).
+#
 func _physics_process(delta):
 	if not Engine.editor_hint:
 		tick(delta)
 
 #
-# Called by _physics_process(). If the character's run cycle is playing, the
-# character runs in "direction" and tries to avoid obstacles. May be overridden
-# by each character subclass.
+# Called by _physics_process(). If the enemy's run cycle is playing, the enemy
+# runs in "direction" and tries to avoid obstacles. May be overridden by each
+# enemy subclass.
 #
 func tick(_delta):
 	if (not $AnimationPlayer.current_animation.ends_with(run_anim)
@@ -101,6 +115,12 @@ func tick(_delta):
 		direction = dir.normalized()
 		$CyclePlayer.set_direction_vector(direction)
 
+#
+# Sets the time scale for playing back animation cycles (higher values to go
+# faster, smaller values to go slower, negative values to go backwards). If
+# speed is 0, the playback speed is not actually set; the animation is paused
+# instead, until the time scale is reset to a non-zero value.
+#
 func set_time_scale(scale = 1.0):
 	var prev_scale = 0 if $CyclePlayer.is_paused() else $CyclePlayer.get_speed()
 	if scale == prev_scale:
@@ -115,18 +135,26 @@ func set_time_scale(scale = 1.0):
 		timer.start(timer.time_left * (prev_scale / scale))
 		$CyclePlayer.set_speed(scale)
 
+#
+# Freezes the enemy in time and space until unfreeze() is called. The enemy
+# flashes briefly, too, unless flash=false.
+#
 func freeze(flash=true):
 	set_time_scale(0)
 	if flash:
 		$Flasher.play("flash")
 
+#
+# Unfreezes the enemy after a call to freeze(). The enemy flashes briefly, too,
+# unless flash=false.
+#
 func unfreeze(flash=true):
 	set_time_scale(1)
 	if flash:
 		$Flasher.play("flash")
 
 #
-# Turns the character to face the alien.
+# Turns the enemy to face the alien, if the alien is visible.
 #
 func face_alien():
 	if is_alien_visible():
@@ -136,8 +164,7 @@ func face_alien():
 		$CyclePlayer.set_direction_vector(direction)
 
 #
-# Called when the alien has been spotted. The default implementation does
-# nothing.
+# Called when the alien has been seen. The default implementation does nothing.
 #
 func alien_seen():
 	pass
@@ -150,7 +177,8 @@ func alien_gone():
 	pass
 
 #
-# Connect the alien detector's "body_entered" signal to this method.
+# Connect the alien detector's "body_entered" signal to this method. When the
+# alien is detected, alien_seen() is called (unless the alien is invisible).
 #
 func on_Alien_Detection_Collider_body_entered(body: Node):
 	if alien == null:
@@ -162,7 +190,9 @@ func on_Alien_Detection_Collider_body_entered(body: Node):
 		alien_seen()
 
 #
-# Connect the alien detector's "body_exited" signal to this method.
+# Connect the alien detector's "body_exited" signal to this method. When the
+# alien leaves the enemy's field of vision, alien_gone() is called (unless the
+# alien is invisible, in which case it makes no difference).
 #
 func on_Alien_Detection_Collider_body_exited(_body: Node):
 	if not $Alien_Detection_Collider/ADCollider.disabled:
@@ -170,6 +200,11 @@ func on_Alien_Detection_Collider_body_exited(_body: Node):
 		if not _alien_invisible_flag:
 			alien_gone()
 
+#
+# This is called when the alien has the invisible talent and makes himself
+# invisible, or when the invisibility wears off. If it makes a difference, the
+# alien_seen() or alien_gone() method is called.
+#
 func _on_alien_invisible(var invisible: bool):
 	if _alien_invisible_flag != invisible:
 		_alien_invisible_flag = invisible
