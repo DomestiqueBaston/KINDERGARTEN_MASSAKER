4c6fd5414d5d50deadd91e715e9c02d092246fdf
Geoff Levner <glevner@gmail.com>
2024-03-06 14:37:02 +0100

    Rewrote code for Binoclard and Blondinet.
    They both use the same script now (HittingEnemy.gd), and they stop attacking
    if the alien moves out of range.

---
+++ GODOT/SCRIPTS/HittingEnemy.gd
@@ -0,0 +1,98 @@
+tool
+extends Enemy
+class_name HittingEnemy
+
+# Enemy subclass for kids who strike the alien (Binoclard and Blondinet). The
+# only ways they differ is in the attacking animation cycle (Kick or Slash) and
+# perhaps the number of hits before they get bored, as well as a few variables
+# from the Enemy class (speed, attack_distance).
+
+# name of the attack animation cycle
+export var attack_animation: String
+
+# number of successful hits before we get bored and go away
+export var max_hit_count := 3
+
+# length in seconds of the attack animation
+var attack_length: float
+
+# are we allowed to attack if we see the alien and get close enough?
+var attack_allowed := true
+
+# number of hits in the current attack
+var hit_count := 0
+
+func _ready():
+	if Engine.editor_hint:
+		return
+	var anim_name = "00_" + attack_animation
+	attack_length = $AnimationPlayer.get_animation(anim_name).length
+
+func on_timer_timeout():
+	if $AnimationPlayer.current_animation.ends_with("_Run"):
+		$CyclePlayer.play(default_anim)
+		start_timer(rand_range(2, 4))
+	else:
+		_start_running()
+		attack_allowed = true
+
+#
+# Play the Run animation cycle 2-5 seconds plus any extra_wait time.
+#
+func _start_running(extra_wait := 0.0):
+	$CyclePlayer.play("Run")
+	start_timer(extra_wait + rand_range(2, 5))
+
+#
+# Stop attacking (at the end of the animation cycle) and start running again. If
+# and_go_away is true, turn around and don't attack again until the next Run
+# cycle ends.
+#
+func _stop_attacking(and_go_away: bool):
+	if and_go_away:
+		turn_around()
+		attack_allowed = false
+	var extra_wait := 0.0
+	if $AnimationPlayer.current_animation.ends_with(attack_animation):
+		extra_wait = attack_length - $AnimationPlayer.current_animation_position
+	_start_running(extra_wait)
+
+#
+# Called by _physics_process().
+#
+func tick(delta):
+	if $CyclePlayer.is_paused():
+		return
+
+	# run towards the alien and stop to attack him when close enough
+
+	if $AnimationPlayer.current_animation.ends_with("_Run"):
+		var attack_him = false
+		if attack_allowed:
+			face_alien()
+			if is_alien_visible() and is_alien_in_range():
+				attack_him = true
+		if attack_him:
+			hit_count = 0
+			$CyclePlayer.play(attack_animation, true)
+			# keep attacking until we have a reason to stop (see below)
+			stop_timer()
+		else:
+			.tick(delta)
+
+	# stop attacking after some number of hits, or if the alien is no no longer
+	# visible and in range
+
+	elif $AnimationPlayer.current_animation.ends_with(attack_animation):
+		if attack_allowed:
+			face_alien()
+			if not (hit_count < max_hit_count
+					and is_alien_visible()
+					and is_alien_in_range()):
+				_stop_attacking(hit_count >= max_hit_count)
+
+#
+# Called when an attack hits the alien.
+#
+func _on_hit(_area: Area2D):
+	hit_count += 1
