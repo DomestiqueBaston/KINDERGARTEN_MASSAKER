ad2270d15421076b087bebd7645d7b6f04c7121c
Geoff Levner <glevner@gmail.com>
2024-03-12 22:19:27 +0100

    Changed X:Y distance ratio from 1:2 to 2:3.

--- GODOT/SCRIPTS/Globals.gd
+++ GODOT/SCRIPTS/Globals.gd
@@ -114,21 +114,21 @@ func get_nearest_direction(dir: Vector2) -> Vector2:
 
 #
 # Returns the square of the distance between two points, adjusting for the
-# game's weird perspective: neighboring pixels are considered to be twice as far
-# away from each other in Y as in X.
+# game's weird perspective: neighboring pixels are considered to be farther
+# away from each other in Y than in X.
 #
 func get_persp_dist_squared(p1: Vector2, p2: Vector2) -> float:
 	var dx = p2.x - p1.x
-	var dy = (p2.y - p1.y) * 2
+	var dy = (p2.y - p1.y) * 1.5
 	return dx*dx + dy*dy
 
 #
 # Returns a velocity vector to go from one point to another at a given speed,
 # but adjusting the speed to the game's weird perspective: characters and
-# projectiles can move twice as fast in X as in Y.
+# projectiles can move faster in X than in Y.
 #
 func get_persp_velocity(from: Vector2, to: Vector2, speed: float) -> Vector2:
 	var vec = from.direction_to(to)
 	var dx = vec.x
-	var dy = vec.y * 0.5
+	var dy = vec.y * 0.667
 	return vec * (speed * sqrt(dx*dx + dy*dy))
