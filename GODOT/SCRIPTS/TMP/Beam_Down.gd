c5e00c6e7b8f0836969636cab4ad26b5ea1368da
DomestiqueBaston <ferdi2x@gmail.com>
2024-03-31 14:14:33 +0200

    Clean up (if there is any issue, I made a backup on my hd even though that's not needed).

--- GODOT/SCRIPTS/TMP/Beam_Down.gd
+++
@@ -1,8 +0,0 @@
-extends KinematicBody2D
-
-
-func _on_AnimationPlayer_animation_finished(_Beam_Down):
-	$"../The Alien (with OUTLINE shader)".visible = true
-	$"../Shadow".visible = true
-	$"../..".set_physics_process(true)
-	queue_free()
